4a3e2d65b4839300d3d5070b7db729d3874d9821
PythonScripts <xielf301@gmail.com>
2016-11-15 18:03:15 +0800

    Update ganglia_nic_rate.sh

--- ganglia_nic_rate.sh
+++ ganglia_nic_rate.sh
@@ -5,23 +5,61 @@
 SECOND=10
 NETWORK=`/sbin/ifconfig|grep -Ev "^ |lo"|awk '{print $1}'|tr -s '\n'|cut -d : -f 1|sort|uniq`
 
-while :
-do
-    for i in $NETWORK
-    do
-        IP=`/sbin/ifconfig $i|awk '/inet /{print $2}'|awk -F ':' '{print $NF}'`
-        if [ -z "$IP" ];then
-            continue
-        fi
+## Get OS Version
+get_os_version(){
+    OS_VERSION=`$(which lsb_release) -r|awk '{print $2}'|cut -d . -f 1`
+    echo "$OS_VERSION"
+}
 
-        rx_before=`/sbin/ifconfig $i|grep "RX bytes"|awk '{print $2}'|cut -c7-`
-        tx_before=`/sbin/ifconfig $i|grep "RX bytes"|awk '{print $6}'|cut -c7-`
-        sleep $SECOND
-        rx_after=`/sbin/ifconfig $i|grep "RX bytes"|awk '{print $2}'|cut -c7-`
-        tx_after=`/sbin/ifconfig $i|grep "RX bytes"|awk '{print $6}'|cut -c7-`
-        rx_result=$(echo "scale=1; ( $rx_after - $rx_before ) / $SECOND" | bc -q)
-        tx_result=$(echo "scale=1; ( $tx_after - $tx_before ) / $SECOND" | bc -q)
-        /usr/bin/gmetric -tuint32 -nnic_rate_rx-"$i" -v"$rx_result" -u"bytes per sec"
-        /usr/bin/gmetric -tuint32 -nnic_rate_tx-"$i" -v"$tx_result" -u"bytes per sec"
-   done
-done
+get_net_interface_rate(){
+    while :
+    do
+        for i in $NETWORK
+        do
+            IP=`/sbin/ifconfig $i|awk '/inet /{print $2}'|awk -F ':' '{print $NF}'`
+            if [ -z "$IP" ];then
+                continue
+            fi
+    
+            rx_before=`/sbin/ifconfig $i|grep "RX bytes"|awk '{print $2}'|cut -c7-`
+            tx_before=`/sbin/ifconfig $i|grep "RX bytes"|awk '{print $6}'|cut -c7-`
+            sleep $SECOND
+            rx_after=`/sbin/ifconfig $i|grep "RX bytes"|awk '{print $2}'|cut -c7-`
+            tx_after=`/sbin/ifconfig $i|grep "RX bytes"|awk '{print $6}'|cut -c7-`
+            rx_result=$(echo "scale=1; ( $rx_after - $rx_before ) / $SECOND" | bc -q)
+            tx_result=$(echo "scale=1; ( $tx_after - $tx_before ) / $SECOND" | bc -q)
+            /usr/bin/gmetric -tuint32 -nnic_rate_rx-"$i" -v"$rx_result" -u"bytes per sec"
+            /usr/bin/gmetric -tuint32 -nnic_rate_tx-"$i" -v"$tx_result" -u"bytes per sec"
+       done
+    done
+}
+
+get_net_interface_rate_7(){
+    while :
+    do
+        for i in $NETWORK
+        do
+            IP=`/sbin/ifconfig $i|awk '/inet /{print $2}'|awk -F ':' '{print $NF}'`
+            if [ -z "$IP" ];then
+                continue
+            fi
+
+            rx_before=`/sbin/ifconfig $i|awk '/RX packets/ {print $5}'`
+            tx_before=`/sbin/ifconfig $i|awk '/TX packets/ {print $5}'`
+            sleep $SECOND
+            rx_after=`/sbin/ifconfig $i|awk '/RX packets/ {print $5}'`
+            tx_after=`/sbin/ifconfig $i|awk '/TX packets/ {print $5}'`
+            rx_result=$(echo "scale=1; ( $rx_after - $rx_before ) / $SECOND" | bc -q)
+            tx_result=$(echo "scale=1; ( $tx_after - $tx_before ) / $SECOND" | bc -q)
+            /usr/bin/gmetric -tuint32 -nnic_rate_rx-"$i" -v"$rx_result" -u"bytes per sec"
+            /usr/bin/gmetric -tuint32 -nnic_rate_tx-"$i" -v"$tx_result" -u"bytes per sec"
+       done
+    done
+}
+
+# --------- main ---------
+if [[ `get_os_version` == 7 ]];then
+    get_net_interface_rate_7
+else
+    get_net_interface_rate
+fi
