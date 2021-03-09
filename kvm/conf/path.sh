1e90853de139b20356a6dff0ba928b5e8cee1d67
PythonScripts <xielf301@gmail.com>
2021-03-09 02:45:11 +0000

    Create path.sh

---
+++ kvm/conf/path.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#变量梳理
+#全局变量
+G_DATE=`date +%Y%m%d`
+G_APPDIR="/home/vmmanager/"
+G_LOGDIR="${G_APPDIR}logs/"
+G_CONF="${G_APPDIR}conf/"
+G_TMP="${G_APPDIR}tmp/"
+G_TEMPLATE="${G_APPDIR}template/"
+G_CONFIG="${G_CONF}config.conf"
+G_VMDIR="/home/VMDATA/"
+G_LIMIT="200M"
